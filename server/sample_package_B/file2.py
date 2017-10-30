c0ed0d61048b469ccf214a8d8e3ec37a58d5eb44
oscar6echo <olivier.borderies@gmail.com>
2017-10-31 00:44:57 +0100

    Add class with attr - TBD

--- server/sample_package_B/file2.py
+++ server/sample_package_B/file2.py
@@ -1,3 +1,18 @@
 def goodbye(x, y, z):
     print('goodbye')
     return x * y * z
+
+class MyClass():
+    """
+    This class has attr - they must be captured too - TBD
+    """
+    arg_1 = "toto"
+    arg_2 = 42
+
+    def __init__(self, arg1, zozo):
+        self.arg_1 = arg1
+        self.zozo = zozo
+    
+    def the_answer_to_everything(self):
+        return self.arg_2
+
